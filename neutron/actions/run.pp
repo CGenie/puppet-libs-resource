54809afc6f5c028d28f1a9436fa604d5c32d0fda
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-06-25 14:36:34 +0200

    Keystone updated, Neutron keystone credentials added

--- neutron/actions/run.pp
+++ neutron/actions/run.pp
@@ -3,13 +3,16 @@ $rabbitmq_user = $resource['input']['rabbitmq_user']['value']
 $rabbitmq_password = $resource['input']['rabbitmq_password']['value']
 $rabbitmq_host = $resource['input']['rabbitmq_host']['value']
 $rabbitmq_port = $resource['input']['rabbitmq_port']['value']
+$keystone_user = $resource['input']['keystone_user']['value']
+$keystone_password = $resource['input']['keystone_password']['value']
+$keystone_tenant = $resource['input']['keystone_tenant']['value']
 
 class { 'neutron':
   debug           => true,
   verbose         => true,
   enabled         => true,
   package_ensure  => 'present',
-  auth_strategy   => 'noauth',
+  auth_strategy   => 'keystone',
   rabbit_user     => $rabbitmq_user,
   rabbit_password => $rabbitmq_password,
   rabbit_host     => $rabbitmq_host,
@@ -20,7 +23,10 @@ class { 'neutron':
 class { 'neutron::server':
   enabled          => true,
   package_ensure   => 'present',
-  auth_type        => 'noauth'
+  auth_type        => 'keystone',
+  auth_password    => $keystone_password,
+  auth_user        => $keystone_user,
+  auth_tenant      => $keystone_tenant
 }
 
 class { 'neutron::agents::dhcp': }
